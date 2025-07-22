31170d3d38c94a126c7bb815e054467470d43004
Pathmavathi240 <pathmavathi975146@gmail.com>
2025-07-22 23:20:44 +0530

    Update broadcast.py

--- DeadlineTech/plugins/misc/broadcast.py
+++ DeadlineTech/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from VILLAIN_MUSIC import app
-from VILLAIN_MUSIC.misc import SUDOERS
-from VILLAIN_MUSIC.utils.database import (
+from DeadlineTech import app
+from DeadlineTech.misc import SUDOERS
+from DeadlineTech.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from VILLAIN_MUSIC.utils.decorators.language import language
-from VILLAIN_MUSIC.utils.formatters import alpha_to_int
+from DeadlineTech.utils.decorators.language import language
+from DeadlineTech.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
