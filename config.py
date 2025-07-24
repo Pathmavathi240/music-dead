6565cf5693c738ad242d46b3337ad5cfeea97669
Pathmavathi240 <pathmavathi975146@gmail.com>
2025-07-24 15:38:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,6 +29,8 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 # Get this value from @Harry_RoxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6848223695))
 
+START_STICKER_ID = getenv("START_STICKER_ID", "https://files.catbox.moe/nr3dvm.mp4")
+
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
