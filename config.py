be73b9b01edf6059007dd2e47ffcd78725737e1a
Pathmavathi240 <pathmavathi975146@gmail.com>
2025-07-25 11:25:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 240))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @Harry_RoxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6848223695))
+OWNER_ID = int(getenv("OWNER_ID", 7478399762))
 
 START_STICKER_ID = getenv("START_STICKER_ID", "https://files.catbox.moe/nr3dvm.mp4")
 
