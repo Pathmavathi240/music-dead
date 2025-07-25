e2f5dfddd1563d5bb7f822efe02f968165fe4fda
Pathmavathi240 <pathmavathi975146@gmail.com>
2025-07-25 14:58:14 +0530

    Update __main__.py

--- DeadlineTech/__main__.py
+++ DeadlineTech/__main__.py
@@ -73,7 +73,7 @@ async def init():
 
     
     for all_module in ALL_MODULES:
-        importlib.import_module("DeadlineTech.plugins" + all_module)
+        importlib.import_module(f"DeadlineTech.plugins.{all_module}")
     LOGGER("DeadlineTech.plugins").info("Successfully Imported Modules...")
     await userbot.start()
     await Anony.start()
