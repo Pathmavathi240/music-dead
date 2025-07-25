061114a4c2fd258cf08becf2d470dc37f5df51d0
Pathmavathi240 <pathmavathi975146@gmail.com>
2025-07-25 15:27:17 +0530

    Update __init__.py

--- DeadlineTech/plugins/__init__.py
+++ DeadlineTech/plugins/__init__.py
@@ -1,26 +1,23 @@
 import glob
+import os
 from os.path import dirname, isfile, join
-
 
 def __list_all_modules():
     work_dir = dirname(__file__)
 
-    # Get modules in plugins/ folder
     mod_paths_main = glob.glob(join(work_dir, "*.py"))
-
-    # Get modules inside subfolders (like tools/*.py)
     mod_paths_sub = glob.glob(join(work_dir, "*/*.py"), recursive=True)
 
     mod_paths = mod_paths_main + mod_paths_sub
 
-    all_modules = [
-        (((f.replace(work_dir, "")).replace("/", ".").replace("\\", "."))[:-3])
-        for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
-    ]
+    all_modules = []
+    for f in mod_paths:
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py"):
+            rel_path = os.path.relpath(f, work_dir).replace("/", ".").replace("\\", ".")
+            module_name = rel_path[:-3]  # .py நீக்கும்
+            all_modules.append(module_name)
 
     return all_modules
-
 
 ALL_MODULES = sorted(__list_all_modules())
 __all__ = ALL_MODULES + ["ALL_MODULES"]
